7b4494003959b2983ce4d74bcd5a3f22d3c8d2ed
Stjepan Puškarić <cipapro@gmail.com>
2021-05-12 17:46:28 +0200

    Tablica programer

--- firmajp24.sql
+++ firmajp24.sql
@@ -4,7 +4,15 @@ use firmajp24;
 
 CREATE TABLE projekt(
 	sifra int NOT NULL PRIMARY KEY auto_increment,
-	naziv varchar(50),
+	naziv varchar(50) not null,
 	cijena decimal(18,2)
+	);
+	
+create table programer(
+	sifra int not null primary key auto_increment,
+	ime varchar(50) not null,
+	prezime varchar(50) not null,
+	datumrodenja datetime,
+	placa decimal(18,2)
 	);
 	
