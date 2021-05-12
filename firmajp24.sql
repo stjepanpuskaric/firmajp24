d0c26a116041fe455666f5d7685fbd0644236c33
Stjepan Puškarić <cipapro@gmail.com>
2021-05-12 17:43:52 +0200

    Tablica projekt

--- firmajp24.sql
+++ firmajp24.sql
@@ -2,3 +2,9 @@ DROP dataabase IF EXISTS firmajp24;
 CREATE database firmajp24;
 use firmajp24;
 
+CREATE TABLE projekt(
+	sifra int NOT NULL PRIMARY KEY auto_increment,
+	naziv varchar(50),
+	cijena decimal(18,2)
+	);
+	
